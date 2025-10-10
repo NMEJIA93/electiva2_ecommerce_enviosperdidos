80d50b2770978dbd95fd4dc3239c8f0a410f260b
NMEJIA93 <norbeymejiacortes@gmail.com>
2025-10-09 21:59:33 -0500

    add service preorden

--- src/domain/services/preorder-services.ts
+++ src/domain/services/preorder-services.ts
@@ -5,8 +5,7 @@ import {validateStock} from './business-rules/preorder-rules';
 
 export const savePreOrder = async (preorderRepo: IPreorderRepository,preorderData: IPreorder, inventoryRepo: IInventoryRepository): Promise<Preorder> => {
     try {
-        // Aquí puedes aplicar reglas de negocio antes de guardar
-        // Ejemplo: validación de stock, cálculo de envío, etc.
+
         await validateStock(preorderData.products, inventoryRepo);
 
         const preorder = new Preorder(preorderData);
@@ -15,4 +14,4 @@ export const savePreOrder = async (preorderRepo: IPreorderRepository,preorderDat
     } catch (error) {
         throw new Error(`[ERROR TO SERVICE] - Error saving preorder: ${error}`);
     }
-};
+}
